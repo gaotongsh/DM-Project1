7090e0d3833559e01827b25c7e1e74b5156e8b88
cyz14 <icyz14@163.com>
2017-06-22 11:37:46 +0800

    完成 bagOfWords 和 wordcloud，修改了stopwords的问题

--- script/preProcessData.R
+++ script/preProcessData.R
@@ -58,10 +58,10 @@ getPreprocessedData <- function() {
     corpus <- Corpus(VectorSource(data$Body))
     # Six Preprocess Items
     corpus <- tm_map(corpus, removePunctuation)
+    corpus <- tm_map(corpus, tolower)
     corpus <- tm_map(corpus, removeWords, stopwords("english"))
     corpus <- tm_map(corpus, removeNumbers)
     corpus <- tm_map(corpus, stripWhitespace)
-    corpus <- tm_map(corpus, tolower)
     corpus <- tm_map(corpus, stemDocument)
     data$Body <- as.list(corpus)
     return(data)
